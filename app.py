69201275534d34a4e31cf15779be1db028bfc187
Jon <jon.westring@gmail.com>
2026-03-25 15:43:39 +0100

    Refactor: compare option for choice 2

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ def main():
         b =float(input("b: "))
         print(add(a,b))
 
+    if option == "2": print(sub(a, b))
+
     if choice == "4" and b == 0: print("Error: division by zero")
     if choice == "4" and b != 0: print(div(a, b))
 
