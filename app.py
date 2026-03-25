134c7e0b8f86b8abf43094f7d1e888c315bf8161
Phil <phigynviv@outlook.com>
2026-03-25 15:53:48 +0100

    Feat: print message on unknown option

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ def main():
     if option == "3": print(mul(a, b))
     if option == "4" and b == 0: print("Error: division by zero")
     if option == "4" and b != 0: print(div(a, b))
+    else: 
+        print("Unknown option")
 
 if __name__ == "__main__":
     main()
