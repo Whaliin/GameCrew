fab38b638a3d9b48cd75d4f4601ce26cf41fd31c
Eren9s <Osama70399@gmail.com>
2026-03-25 15:42:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ def main():
     print("0) Exit")
     option = input("select: ")
     
-    if choice == "1": 
+    if option == "1": 
         a = float(input("a: "))    
         b =float(input("b: "))
         print(add(a,b))
 
-    if choice == "4" and b == 0: print("Error: division by zero")
-    if choice == "4" and b != 0: print(div(a, b))
+    if option == "4" and b == 0: print("Error: division by zero")
+    if option == "4" and b != 0: print(div(a, b))
 
 if __name__ == "__main__":
     main()
