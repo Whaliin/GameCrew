058a4370dc6c95c78c2116648b123df9f6706ecb
Jon <jon.westring@gmail.com>
2026-04-20 18:53:23 +0200

    merged backend from example

--- app/routers/pages.py
+++ app/routers/pages.py
@@ -53,7 +53,35 @@ def game_page(request: Request, game_slug: str):
 @router.get("/profile/{user_id}", response_class=HTMLResponse)
 def profile_page(request: Request, user_id: str):
 	context = build_page_context_stub("profile")
-	
-	context["user_id"] = user_id
+
+	# Exempeldata för testning
+	context["profile"] = {
+		"username": user_id,
+		"user_tag": f"#{user_id}",
+		"avatar_url": "/static/img/profiles/default.jpg",
+		"rank": "Global Elite",
+		"age_range": "18-25",
+		"platform": "PC",
+		"playtime": "Kvall",
+		"languages": "SV / EN",
+		"bio": "Passionerad FPS-spelare. Soker seriost lag for ranked!",
+	}
+
+	context["nav_games"] = [
+		{"name": "Counter-Strike 2", "slug": "cs2", "image_url": "/static/img/games/csgo.jpg"},
+		{"name": "League of Legends", "slug": "lol", "image_url": "/static/img/games/lol.jpg"},
+		{"name": "Valorant", "slug": "valorant", "image_url": "/static/img/games/valorant.jpg"},
+		{"name": "ARC Raiders", "slug": "arcraiders", "image_url": "/static/img/games/arcraiders.jpg"},
+		{"name": "Mobile Legends", "slug": "mobilelegends", "image_url": "/static/img/games/mobilelegends.jpg"},
+	]
+
+	context["profile_games"] = [
+		{"name": "Counter-Strike 2", "slug": "cs2", "image_url": "/static/img/games/csgo.jpg"},
+		{"name": "League of Legends", "slug": "lol", "image_url": "/static/img/games/lol.jpg"},
+		{"name": "Valorant", "slug": "valorant", "image_url": "/static/img/games/valorant.jpg"},
+		{"name": "Mobile Legends", "slug": "mobilelegends", "image_url": "/static/img/games/mobilelegends.jpg"},
+		{"name": "ARC Raiders", "slug": "arcraiders", "image_url": "/static/img/games/arcraiders.jpg"},
+		{"name": "Apex Legends", "slug": "apex", "image_url": "/static/img/games/apexlegends.jpg"},
+	]
 
 	return templates.TemplateResponse(request=request, name="profile.html", context=context)
